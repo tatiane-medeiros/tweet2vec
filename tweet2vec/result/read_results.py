c2e6072be65020ff8dc7e1c7ab799cf6d3c8df02
Maria Tatiane <tatianemedeiros@ic.ufal.br>
2022-12-09 11:29:38 -0300

    add data

---
+++ tweet2vec/result/read_results.py
@@ -0,0 +1,20 @@
+import numpy as np
+import _pickle as pkl
+
+##f = open('data.pkl', 'rb')
+##data = pkl.load(f)
+##f.close()
+##f = open('targets.pkl', 'rb')
+##targets = pkl.load(f)
+##f.close()
+##
+##for i in range(len(targets)):
+##    print(targets[i], '-', data[i])
+
+
+f = open('readable.txt', 'r')
+a = f.read()
+f.close()
+b = [x.split('\t') for x in a.split('\n')]
+
+validation = pd.DataFrame(b, columns=['un. medida','predicted','texto'])
