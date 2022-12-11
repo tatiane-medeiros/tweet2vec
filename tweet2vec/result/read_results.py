d5f8504b538fdec493284108f2d4bad8846be88b
Maria Tatiane <tatianemedeiros@ic.ufal.br>
2022-12-11 00:14:35 -0300

    results

--- tweet2vec/result/read_results.py
+++ tweet2vec/result/read_results.py
@@ -2,20 +2,45 @@ import numpy as np
 import _pickle as pkl
 import pandas as pd
 
-##f = open('data.pkl', 'rb')
-##data = pkl.load(f)
-##f.close()
-##f = open('targets.pkl', 'rb')
-##targets = pkl.load(f)
-##f.close()
-##
-##for i in range(len(targets)):
-##    print(targets[i], '-', data[i])
+def targets():
+    f = open('data.pkl', 'rb')
+    data = pkl.load(f)
+    f.close()
+    f = open('targets.pkl', 'rb')
+    targets = pkl.load(f)
+    f.close()
+    table = []
+    for i in range(len(targets)):
+       table.append([targets[i], data[i]])
 
+    return table
 
-f = open('readable.txt', 'r')
-a = f.read()
-f.close()
-b = [x.split('\t') for x in a.split('\n')]
+def test_table():
 
-validation = pd.DataFrame(b, columns=['un. medida','predicted','texto'])
+    f = open('readable.txt', 'r')
+    a = f.read()
+    f.close()
+    b = [x.split('\t') for x in a.split('\n')]
+
+    validation = pd.DataFrame(b, columns=['un. medida','predicted','texto'])
+    validation.sort_values('un. medida').to_csv('result.csv', index=False)
+
+def predicted_table():
+    f = open('predicted_tags.txt', 'r')
+    a = f.read()
+    f.close()
+    a = a.split('\n')
+
+    f = open('../../data/data.txt', 'r')
+    b = f.read()
+    f.close()
+    c = [x.split('\t') for x in b.split('\n')]
+    data = []
+    for i in range(len(c)):
+        data.append([a[i]] + c[i])
+
+    return data
+
+d = predicted_table()
+predict = pd.DataFrame(d, columns=['predicted','unid. real','texto'])
+predict.sort_values('unid. real').to_csv('predicted.csv', index=False)
